328cd03164c8cfa8444a675ceb033e58a870f02b
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-02 21:32:50 +0530

    ADDED Code to calculate employee wage for part time employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,12 +1,15 @@
 console.log("Welcome to employee wage program.\n");
 
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 let EMP_RATE_PER_HOUR = 20;
 
 let empHrs = 0;
 let empWage = 0;
-let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == 1)
+let empCheck = Math.floor(Math.random() * 10) % 3;
+if (empCheck == IS_PART_TIME)
+    empHrs = 4;
+else if (empCheck == IS_FULL_TIME)
     empHrs = 8;
 else
     empHrs = 0;
