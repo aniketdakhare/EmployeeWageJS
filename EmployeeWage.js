750fb63bc87a36b79e0ff616f4a1c29bb63b575d
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-02 22:20:51 +0530

    ADDED Code to calculate employee wage for 20 days or 100 hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,25 +4,28 @@ let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
 let EMP_RATE_PER_HOUR = 20;
 let NUM_OF_WORKING_DAYS = 20;
+let MAX_HRS_IN_MONTH = 100;
 
 let empHrs = 0;
 let empWage = 0;
-let monthlyWage = 0;
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++)
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
 {
+	totalWorkingDays++;
 	let empCheck = Math.floor(Math.random() * 10) % 3;
 	switch(empCheck)
 	{
 		case IS_PART_TIME:
 			empHrs = 4;
-         break
+         break;
     	case IS_FULL_TIME:
          empHrs = 8;
-         break
+         break;
     	default:
          empHrs = 0;
    }
-   empWage = empHrs * EMP_RATE_PER_HOUR;
-   monthlyWage += empWage;
+	totalEmpHrs += empHrs;
 }
-console.log("Employee Wage Per Month = " + monthlyWage);
+empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+console.log("Employee Wage Per Month = " + empWage);
